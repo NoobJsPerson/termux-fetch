0f6b2101af1db37da783892a1870388bf3d18edf
Amine El Baghdadi <75886237+NoobJsPerson@users.noreply.github.com>
2021-07-29 20:52:56 +0100

    Update termux-fetch.sh

--- termux-fetch.sh
+++ termux-fetch.sh
@@ -2,4 +2,4 @@ yellow=$(printf '\033[1;33m')
 underline=$(printf '\033[1;4;93m')
 normal=$(printf '\033[0m')
 termux-camera-info | jq -r --arg yellow $yellow  --arg normal $normal --arg  underline $underline '"\($underline)General Info:\($normal)\n\($yellow)Cameras: \($normal)\(length)"'
-termux-battery-status | jq -r --arg yellow $yellow  --arg normal $normal --arg  underline $underline '"\($underline)Battery Info:\($normal)\n\($yellow)Health: \($normal)\(.health)\n\($yellow)Percentage: \($normal)\(.percentage | tostring)%\n\($yellow)Temperature: \($normal)\(.temperature | tostring)"
+termux-battery-status | jq -r --arg yellow $yellow  --arg normal $normal --arg  underline $underline '"\($underline)Battery Info:\($normal)\n\($yellow)Health: \($normal)\(.health)\n\($yellow)Percentage: \($normal)\(.percentage | tostring)%\n\($yellow)Temperature: \($normal)\(.temperature | tostring)"'
