54e7e441ffae960176f9d7d8217ff27d7e104454
Amine El Baghdadi <75886237+NoobJsPerson@users.noreply.github.com>
2021-08-02 00:24:16 +0100

    Adds camera capabilities

--- termux-fetch.sh
+++ termux-fetch.sh
@@ -1,5 +1,5 @@
 yellow=$(printf '\033[1;33m')
 underline=$(printf '\033[1;4;93m')
 normal=$(printf '\033[0m')
-termux-camera-info | jq -r --arg yellow $yellow  --arg normal $normal --arg  underline $underline '"\($underline)General Info:\($normal)\n\($yellow)Cameras: \($normal)\(length)"'
+termux-camera-info | jq -r --arg yellow $yellow  --arg normal $normal --arg  underline $underline '"\($underline)General Info:\($normal)\n\($yellow)Cameras: \($normal)\(length)\n\($yellow)Capabilities: \($normal)\(.[0].capabilities | join(", "))"'
 termux-battery-status | jq -r --arg yellow $yellow  --arg normal $normal --arg  underline $underline '"\($underline)Battery Info:\($normal)\n\($yellow)Health: \($normal)\(.health)\n\($yellow)Percentage: \($normal)\(.percentage | tostring)%\n\($yellow)Temperature: \($normal)\(.temperature | tostring)"'
