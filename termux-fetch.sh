527a2465c300d4ceff2b558eecb6998c3326c54e
Amine El Baghdadi <75886237+NoobJsPerson@users.noreply.github.com>
2021-08-02 23:54:49 +0100

    Added termux version

--- termux-fetch.sh
+++ termux-fetch.sh
@@ -2,12 +2,13 @@ yellow=$(printf '\033[1;33m')
 underline=$(printf '\033[1;4;93m')
 normal=$(printf '\033[0m')
 termux-camera-info | jq -r --arg yellow $yellow  --arg normal $normal --arg  underline $underline '"\($underline)General Info:\($normal)\n\($yellow)Cameras: \($normal)\(length)\n\($yellow)Capabilities: \($normal)\(.[0].capabilities | join(", "))"'
-termux-battery-status | jq -r --arg yellow $yellow  --arg normal $normal --arg  underline $underline '"\($underline)Battery Info:\($normal)\n\($yellow)Health: \($normal)\(.health)\n\($yellow)Percentage: \($normal)\(.percentage | tostring)%\n\($yellow)Temperature: \($normal)\(.temperature | tostring)"'
+termux-battery-status | jq -r --arg yellow $yellow  --arg normal $normal --arg  underline $underline '"\($underline)Battery Info:\($normal)\n\($yellow)Health: \($normal)\(.health)\n\($yellow)Percentage: \($normal)\(.percentage | tostring)%\n\($yellow)Temperature: \($normal)\(.temperature | floor | tostring)°C"'
 IFS=' ' read -ra array <<< "$(uptime)"
 uptimestr=${array[2]}
 if [ "${array[3]}" = "min," ]
-then uptimestr+="min(s)"
+then uptimestr+= "min(s)"
 else uptimestr+="hour(s)"
 fi
 echo "${underline}General Info:${normal}
-${yellow}Uptime: ${normal}${uptimestr/,/' '}"
+${yellow}Uptime: ${normal}${uptimestr/,/' '}
+${yellow}Termux Version: ${normal}${TERMUX_VERSION}"
