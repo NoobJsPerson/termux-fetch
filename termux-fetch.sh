c76a0d2889b25dbf45fd0998b54e2a36816fe8ec
Amine El Baghdadi <75886237+NoobJsPerson@users.noreply.github.com>
2021-08-02 23:14:32 +0100

    Update termux-fetch.sh

--- termux-fetch.sh
+++ termux-fetch.sh
@@ -3,3 +3,11 @@ underline=$(printf '\033[1;4;93m')
 normal=$(printf '\033[0m')
 termux-camera-info | jq -r --arg yellow $yellow  --arg normal $normal --arg  underline $underline '"\($underline)General Info:\($normal)\n\($yellow)Cameras: \($normal)\(length)\n\($yellow)Capabilities: \($normal)\(.[0].capabilities | join(", "))"'
 termux-battery-status | jq -r --arg yellow $yellow  --arg normal $normal --arg  underline $underline '"\($underline)Battery Info:\($normal)\n\($yellow)Health: \($normal)\(.health)\n\($yellow)Percentage: \($normal)\(.percentage | tostring)%\n\($yellow)Temperature: \($normal)\(.temperature | tostring)"'
+IFS=' ' read -ra array <<< "$(uptime)"
+uptimestr=${array[2]}
+if [ "${array[3]}" = "min," ]
+then uptimestr+="min(s)"
+else uptimestr+="hour(s)"
+fi
+echo "${underline}General Info:${normal}
+${yellow}Uptime: ${normal}${uptimestr/,/' '}"
