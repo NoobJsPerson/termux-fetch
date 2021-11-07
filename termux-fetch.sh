28b860b2c4c69f864033a039f95f5ef89f17a88c
NoobJsPerson <itsmeac101.gmail.com>
2021-11-07 13:11:24 +0100

    add tts support

--- termux-fetch.sh
+++ termux-fetch.sh
@@ -1,19 +1,29 @@
-toGB(){	
+#!/usr/bin/bash
+toGB(){
 	awk "BEGIN{print $(simplify $1)/1048576 }" | cut -c1-4
 }
 simplify(){
 	echo $1 | cut -d'k' -f 1
 }
+checkcmd(){
+	ls $PREFIX/bin | grep -c $1
+}
+main(){
 sh <<< 'top > .tmp'
 marray=($(cat .tmp | tr ';' '\n'))
 rm .tmp
+if [[ $1 != t ]]; then
 yellow=$(printf '\033[1;33m')
 underline=$(printf '\033[1;4;93m')
 normal=$(printf '\033[0m')
-if [[ $(ls $PREFIX/bin | grep -c 'termux-camera-info') != 0 && $(ls $PREFIX/bin | grep -c 'jq') != 0 ]];
+else
+yellow='the'
+normal='is'
+fi
+if [[ $(checkcmd termux-camera-info) != 0 && $(checkcmd jq) != 0 ]];
 then
-termux-camera-info | jq -r --arg yellow $yellow  --arg normal $normal --arg  underline $underline '"\($yellow)Cameras: \($normal)\(length)\n\($yellow)Capabilities: \($normal)\(.[0].capabilities | join(", "))"'
-termux-battery-status | jq -r --arg yellow $yellow  --arg normal $normal --arg  underline $underline '"\($yellow)Health: \($normal)\(.health)\n\($yellow)Percentage: \($normal)\(.percentage | tostring)%\n\($yellow)Temperature: \($normal)\(.temperature | floor | tostring)°C"'
+termux-camera-info | jq -r --arg yellow $yellow  --arg normal $normal '"\($yellow)Cameras: \($normal)\(length)\n\($yellow)Capabilities: \($normal)\(.[0].capabilities | join(", "))"'
+termux-battery-status | jq -r --arg yellow $yellow  --arg normal $normal '"\($yellow)Health: \($normal)\(.health)\n\($yellow)Percentage: \($normal)\(.percentage | tostring)%\n\($yellow)Temperature: \($normal)\(.temperature | floor | tostring)°C"'
 fi
 echo "${yellow}OS: ${normal}$(uname -o) $(getprop ro.build.version.release) $(uname -m)
 ${yellow}Host: ${normal}$(getprop ro.product.brand) $(getprop ro.vendor.product.model)
@@ -21,3 +31,6 @@ ${yellow}Kernel: ${normal}$(uname -rs)
 ${yellow}Uptime:${normal}$(uptime -p | cut -d'p' -f 2)
 ${yellow}Memory: ${normal}$(toGB $(($(simplify ${marray[15]}) - $(simplify ${marray[19]}))))GB / $(toGB ${marray[13]})GB
 ${yellow}Termux Version: ${normal}${TERMUX_VERSION}"
+}
+getopts t o
+[[ $(checkcmd termux-tts-speak) != 0 && $o != '?' ]] && main t | termux-tts-speak || main
