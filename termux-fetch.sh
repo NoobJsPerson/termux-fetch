a51d821a91f534a77ee483ac1ad1afe840a01898
NoobJsPerson <itsmeac101@gmail.com>
2023-12-02 20:55:04 +0100

    use getopt and add disable camera info and disable battery status

--- termux-fetch.sh
+++ termux-fetch.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/bash
+TEMP=`getopt -o a:b:cdht --long disable-camera-info,disable-battery-status,help,ascii,brand,tts -- "$@"`
+eval set -- "$TEMP"
+noop(){
+	printf ''
+}
 toGB(){
 	awk "BEGIN{print $(simplify $1)/1048576 }" | cut -c1-4
 }
@@ -11,12 +16,12 @@ checkcmd(){
 main(){
 marray=($(free | tr ';' '\n'))
 ascii=''
-if [[ $1 != t ]]; then
+if [[ $1 != *t* ]]; then
 y=$(printf '\033[1;33m')
 n=$(printf '\033[0m')
 w=$(printf '\033[47m')
-[[ $1 == b ]] && brand=$2 || brand=$(getprop ro.product.brand)
-if [[ $1 == a ]]; then
+[[ $1 == *b* ]] && brand=$2 || brand=$(getprop ro.product.brand)
+if [[ $1 == *a* ]]; then
 ascii=$(printf "$(cat $2)")"
 
 "
@@ -54,7 +59,7 @@ ${g}                                         ${n}
     		;;
 		*)
 		ascii="
-    __
+    ___
     \##\\
      \##\\
       \##\\
@@ -91,45 +96,71 @@ esac
 echo "${ascii}${y}$(whoami)@$(hostname)"
 if [[ $(checkcmd termux-camera-info) != 0 && $(checkcmd jq) != 0 ]];
 then
-termux-camera-info | jq -r --arg y $y  --arg n $n '"\($y)Cameras: \($n)\(length)\n\($y)Capabilities: \($n)\(.[0].capabilities | join(", "))"'
-termux-battery-status | jq -r --arg y $y  --arg n $n '"\($y)Battery Health: \($n)\(.health)\n\($y)Percentage: \($n)\(.percentage | tostring)%\n\($y)Temperature: \($n)\(.temperature | floor | tostring)°C"'
+[[ $1 != *c* ]] && termux-camera-info | jq -r --arg y $y  --arg n $n '"\($y)Cameras: \($n)\(length)\n\($y)Capabilities: \($n)\(.[0].capabilities | join(", "))"' || noop
+[[ $1 != *d* ]] && termux-battery-status | jq -r --arg y $y  --arg n $n '"\($y)Battery Health: \($n)\(.health)\n\($y)Percentage: \($n)\(.percentage | tostring)%\n\($y)Temperature: \($n)\(.temperature | floor | tostring)°C"' || noop
+fi
+if [[ $(getprop vzw.os.rooted) == true ]]; then
+rooted="Rooted"
 fi
 echo "${y}OS: ${n}$(uname -o) $(getprop ro.build.version.release) $(uname -m)
-${y}Host: ${n}${brand} $(getprop ro.vendor.product.model)
+${y}Host: ${n}${brand} $(getprop ro.vendor.product.model) ${rooted}
 ${y}Kernel: ${n}$(uname -rs)
 ${y}Uptime:${n}$(uptime -p | cut -d'p' -f 2)
 ${y}Shell: ${n}${shell}
 ${y}Packages: ${n}$((apt list --installed | wc -l) 2> /dev/null)
 ${y}Memory: ${n}$(toGB $(simplify ${marray[12]}))GB / $(toGB ${marray[7]})GB
-${y}Termux Version: ${n}${TERMUX_VERSION}"
+${y}Termux Version: ${n}${TERMUX_VERSION}
+${extra}"
 }
 if [[ $1 != '' ]]; then
- while getopts "atbh" OPTIONS; do
-	case $OPTIONS in
-		a)
-		[[ $2 != '' ]] && main a $2 || (echo "You didn't provide a file (using default ascii)" && main)
-		exit 0;
+for ((i=1; i<=$#; i++)) do
+	j=$(expr ${i} - 1)
+	case "${!i}" in
+		-a| --ascii)
+		if [[ "${!j}" != '' ]]; then
+		 param1="${param1}a"
+		 param2="${!j}"
+		else
+		 echo "You didn't provide a file (using default ascii)"
+		fi;
 		;;
-		t)
-		[[ $(checkcmd termux-tts-speak) != 0 ]] && main t | termux-tts-speak || main
-		exit 0;
+		-t|--tts)
+		if [[ $(checkcmd termux-tts-speak) != 0 ]]; then param1="${param1}t"
+		fi
 		;;
-		b)
-		[[ $2 != '' ]] && main b $2 || (echo "You didn't provide a brand (using your device's brand)" && main)
-		exit 0
+		-b|--brand)
+		if [[ "${!j}" != '' ]]; then
+		param1="${param1}b"
+		param2="${!j}"
+		else
+		echo "You didn't provide a brand (using your device's brand)"
+		fi
 		;;
-		h)
+		-c|--disable-camera-info)
+		param1="${param1}c"
+		param2="${!j}"
+		;;
+		-d|--disable-battery-status)
+		param1="${param1}d"
+		param2="${!j}"
+		;;
+		-h|--help)
 		echo "termux-fetch: a simple bash script that displays information about your device like linux version, device brand and model, default termux shell and more.
-you can even allow the script to retrieve more information like camera count and features, battery status and device temperature by installing the Termux:API app from F-droid (https://f-droid.org/en/packages/com.termux.api/) and installing the packages (termux-api) and (jq), by doing so you also can choose to hear the output of this script be read by a tts voice using the -t flag
+you can even allow the script to retrieve more information like camera count and features, battery status and device temperature by installing the Termux:API app from F-droid (https://f-droid.org/en/packages/com.termux.api/) and installing the packages (termux-api) and (jq), by doing so you unlock the ability to hear the output of this script be read by a tts voice using the -t flag
 Options:
 
-		-t: read script output with tts voice
+		-t, --tts: reads script output with tts voice (requires Termux:API app and termux-api and jq packages)
 
-		-h: display this help message
+		-h, --help: displays this help message
 
-		-a: display content of the given file instead of ascii art
+		-a, --ascii: displays content of the given file instead of ascii art
 
-		-b: display the ascii art of the given brand (has higher priority than -a)
+		-b, --brand: displays the ascii art of the given brand (has higher priority than -a)
+
+		-c, --disable-camera-info: disables showing camera informations
+
+		-d, --disable-battery-info: disables showing battery status
+
 "
 		exit 0;
 		;;
@@ -139,6 +170,7 @@ Options:
 		;;
 	esac
 done
+[[ $param1 == *t* ]] && main $param1 $param2 | termux-tts-speak || main $param1 $param2
 else
  main
 fi
